76f6f92bfb8b5c535557cf772ee7b1141f0c7fa8
Gustavo Brunetto <gustavobruneto@gmail.com>
2017-04-07 19:01:36 -0300

    implement functionality to + and - button

--- app/src/main/java/com/example/gusbru/justjava/MainActivity.java
+++ app/src/main/java/com/example/gusbru/justjava/MainActivity.java
@@ -1,13 +1,35 @@
 package com.example.gusbru.justjava;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    int quantity = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void incrementQuantity(View view) {
+        quantity += 1;
+        displayQuantity();
+    }
+
+    public void decrementQuantity(View view) {
+        if (quantity>0) {
+            quantity -= 1;
+        }
+        displayQuantity();
+    }
+
+    private void displayQuantity() {
+        TextView quantityTextView = (TextView) findViewById(R.id.quantity_label);
+        quantityTextView.setText(String.valueOf(quantity));
+    }
+
 }
